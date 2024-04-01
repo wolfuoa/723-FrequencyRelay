59ec98f98db60ed34dfe088cf958209eeb60d130
wolfuoa <nwol626@aucklanduni.ac.nz>
2024-04-01 23:21:56 +1300

    feat: :sparkles: Abstracted Timers and Alarms

--- software/src/main.c
+++ software/src/main.c
@@ -3,57 +3,35 @@
  * Household Frequency Relay
  * 		Author: Nicholas Wolf, Robert Sefaj, Janith Hettiarachchi
  *
- * This project sees through the development of a household frequency relay capable of
- * disconnecting loads when power singal conditions
+ * This project sees through the development of a household frequency relay
+ * capable of disconnecting loads when power singal conditions
  */
 
-#include <stdio.h>
-#include "system.h"
-#include "altera_avalon_pio_regs.h"
 #include "../software/FreeRTOS/FreeRTOS.h"
 #include "../software/FreeRTOS/task.h"
-#include "../software/FreeRTOS/timers.h"
+#include "altera_avalon_pio_regs.h"
+#include "system.h"
+#include <stdio.h>
 
+#include "timer.h"
 
-#define Timer_Reset_Task_P      (tskIDLE_PRIORITY+1)
 TimerHandle_t timer;
 TaskHandle_t Timer_Reset;
 
+int main() {
 
-void Timer_Reset_Task(void *pvParameters ){ //reset timer if any of the push button is pressed
-	while(1){
-		if (IORD_ALTERA_AVALON_PIO_DATA(PUSH_BUTTON_BASE) != 0x7){
-			xTimerReset( timer, 10 );
-		}
+  timer = xTimerCreate("Timer Name", 1000, pdTRUE, NULL, vTimerCallback);
 
-	}
-}
+  if (xTimerStart(timer, 0) != pdPASS) {
+    printf("ERROR: Failed to start timer");
+    return 1;
+  }
 
+  vTaskStartScheduler();
 
-void vTimerCallback(xTimerHandle t_timer){ //Timer flashes green LEDs
-	IOWR_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE, 0xFF^IORD_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE));
-}
-
-
-int main()
-{
-
-
-	IOWR_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE, 0x55);
-
-	timer = xTimerCreate("Timer Name", 1000, pdTRUE, NULL, vTimerCallback);
-
-	if (xTimerStart(timer, 0) != pdPASS){
-		printf("Cannot start timer");
-	}
-	xTaskCreate( Timer_Reset_Task, "0", configMINIMAL_STACK_SIZE, NULL, Timer_Reset_Task_P, &Timer_Reset );
-
-	vTaskStartScheduler();
-
-	// Program will never reach this point
-	while(1){
-
-	}
+  // Program will never reach this point
+  while (1) {
+  }
 
   return 0;
 }
