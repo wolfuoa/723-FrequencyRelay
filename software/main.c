0158fc25c5b31ba9399280bf4954bf1116b775e4
Robert Sefaj <rsef899@UoA.auckland.ac.nz>
2024-04-04 19:54:26 +1300

    Created Semaphore for the semaphore reading TODO: print freq value from the task

--- software/main.c
+++ software/main.c
@@ -4,9 +4,14 @@
 #include <string.h>
 
 /* Scheduler includes. */
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
+#include "FreeRTOS/FreeRTOS.h"
+#include "FreeRTOS/task.h"
+#include "FreeRTOS/queue.h"
+#include "FreeRTOS/semphr.h"
+
+#include "sys/alt_irq.h"
+#include "io.h"
+#include "altera_avalon_pio_regs.h"
 
 /* The parameters passed to the reg test tasks.  This is just done to check
  the parameter passing mechanism is working correctly. */
@@ -15,12 +20,26 @@
 #define mainREG_TEST_PRIORITY       ( tskIDLE_PRIORITY + 1)
 static void prvFirstRegTestTask(void *pvParameters);
 static void prvSecondRegTestTask(void *pvParameters);
+SemaphoreHandle_t freq_semaphore;
+void freq_relay(){
+	//unsigned int temp = IORD(FREQUENCY_ANALYSER_BASE, 0);
+	//printf("%f Hz\n", 16000/(double)temp);
+	BaseType_t handle = pdFALSE;
+	xSemaphoreGiveFromISR(freq_semaphore,&handle);
+
+	return;
+}
 
 /*
  * Create the demo tasks then start the scheduler.
  */
 int main(void)
 {
+	alt_irq_register(0x43100, 0, freq_relay);
+
+	freq_semaphore = xSemaphoreCreateBinary();
+
+
 	/* The RegTest tasks as described at the top of this file. */
 	xTaskCreate( prvFirstRegTestTask, "Rreg1", configMINIMAL_STACK_SIZE, mainREG_TEST_1_PARAMETER, mainREG_TEST_PRIORITY, NULL);
 	xTaskCreate( prvSecondRegTestTask, "Rreg2", configMINIMAL_STACK_SIZE, mainREG_TEST_2_PARAMETER, mainREG_TEST_PRIORITY, NULL);
@@ -38,6 +57,9 @@ static void prvFirstRegTestTask(void *pvParameters)
 	{
 		printf("Task 1\n");
 		vTaskDelay(1000);
+		if (xSemaphoreTake(freq_semaphore,portMAX_DELAY) == pdTRUE) {
+			printf("found\n");
+		}
 	}
 
 }
